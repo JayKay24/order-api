422da281d9c36a83f445f5bb46efc5fe50e3ab24
James Kinyua <jameskinyua590@gmail.com>
2019-08-05 22:54:44 +0300

    refactor: replace jest test runner with mocha and chai

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,15 +1,21 @@
 module.exports = function(config) {
   config.set({
-    mutator: "typescript",
-    packageManager: "yarn",
-    reporters: ["html", "progress", "dashboard"],
-    testRunner: "jest",
-    transpilers: ["typescript"],
-    coverageAnalysis: "off",
-    jest: {
-      config: require('./jest.config')
+    testRunner: 'mocha',
+    mochaOptions: {
+      files: ['test/**/*.spec.ts'],
+      opts: './test/mocha.opts',
+      ui: 'bdd',
+      timeout: 35000,
+      require: ['ts-node/register', 'source-map-support/register'],
+      asyncOnly: false,
     },
-    tsconfigFile: "tsconfig.json",
-    mutate: ["src/**/*.ts"]
-  });
-};
+    mutator: 'typescript',
+    transpilers: [],
+    reporters: ['html', 'progress', 'dashboard'],
+    packageManager: 'npm',
+    testFramework: 'mocha',
+    coverageAnalysis: 'off',
+    tsconfigFile: 'tsconfig.json',
+    mutate: ['src/**/*.ts'],
+  })
+}
