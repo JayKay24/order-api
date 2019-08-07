8a13f62d2549dfada97134e5c8a8dfeea340a42c
James Kinyua <jameskinyua590@gmail.com>
2019-08-07 16:48:26 +0300

    refactor: add pagination capabilities to the orders endpoint

--- order-api/src/controllers/order.ts
+++ order-api/src/controllers/order.ts
@@ -12,16 +12,13 @@ export let getOrder = (req: Request, res: Response, next: NextFunction) => {
   return res.status(httpStatusCode).send(order)
 }
 
-// export let getAllOrders = (req: Request, res: Response, next: NextFunction) => {
-//   const limit = req.query.limit || orders.length
-//   const offset = req.query.offset || 0
-//   return res.status(200).send(
-//     _(orders)
-//       .drop(offset)
-//       .take(limit)
-//       .value()
-//   )
-// }
+export let getAllOrders = (req: Request, res: Response, next: NextFunction) => {
+  const limit = req.query.limit || orders.length
+  const offset = req.query.offset || 0
+  const dropped = _.drop(orders, offset)
+  const taken = _.take(dropped, limit)
+  return res.status(200).send(taken)
+}
 
 export let addOrder = (req: Request, res: Response, next: NextFunction) => {
   const order: Order = {
