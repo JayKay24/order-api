953851fb1adf3e671bc18f80459e284c781086d2
James Kinyua <jameskinyua590@gmail.com>
2019-08-07 16:48:26 +0300

    refactor: add pagination capabilities to the orders endpoint

--- order-api/src/routes/order.ts
+++ order-api/src/routes/order.ts
@@ -5,7 +5,7 @@ export class OrderRoute {
   public routes(app: express.Application): void {
     app.route('/store/inventory').get(orderController.getInventory)
     app.route('/store/orders').post(orderController.addOrder)
-    // app.route('/store/orders').get(orderController.getAllOrders)
+    app.route('/store/orders').get(orderController.getAllOrders)
     app.route('/store/orders/:id').get(orderController.getOrder)
     app.route('/store/orders/:id').delete(orderController.removeOrder)
   }
