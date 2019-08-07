9e742b3f88ff834a25276e0746feaae7eea38313
James Kinyua <jameskinyua590@gmail.com>
2019-08-07 09:04:32 +0300

    chore: add unit tests for the order and user models

--- order-api/src/app.ts
+++ order-api/src/app.ts
@@ -1,16 +1,16 @@
-import * as bodyParser from 'body-parser';
-import express from 'express';
-import { Index } from './routes/index';
+import * as bodyParser from 'body-parser'
+import express from 'express'
+import { Index } from './routes/index'
 
 class App {
-  public app: express.Application;
-  public indexRoutes: Index = new Index();
+  public app: express.Application
+  public indexRoutes: Index = new Index()
 
   constructor() {
-    this.app = express();
-    this.app.use(bodyParser.json());
-    this.indexRoutes.routes(this.app);
+    this.app = express()
+    this.app.use(bodyParser.json())
+    this.indexRoutes.routes(this.app)
   }
 }
 
-export default new App().app;
+export default new App().app
