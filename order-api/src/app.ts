8f886151e8da8c91d0a13861d21ede27a37d7dcc
James Kinyua <jameskinyua590@gmail.com>
2019-08-16 13:28:45 +0300

    refactor: replace the in-memory array database with mongodb

--- order-api/src/app.ts
+++ order-api/src/app.ts
@@ -1,5 +1,6 @@
 import * as bodyParser from 'body-parser';
 import express from 'express';
+import mongoose from 'mongoose';
 import { APIRoute } from './routes/api';
 import { OrderRoute } from './routes/order';
 import { UserRoute } from './routes/user';
@@ -9,6 +10,7 @@ class App {
   public apiRoutes: APIRoute = new APIRoute();
   public orderRoutes: OrderRoute = new OrderRoute();
   public userRoutes: UserRoute = new UserRoute();
+  public mongoUrl: string = 'mongodb://localhost/order-api'
 
   constructor() {
     this.app = express();
@@ -16,6 +18,14 @@ class App {
     this.apiRoutes.routes(this.app);
     this.orderRoutes.routes(this.app);
     this.userRoutes.routes(this.app);
+    this.mongoSetUp();
+  }
+
+  private mongoSetUp(): void {
+    mongoose.connect(
+      this.mongoUrl,
+      { useNewUrlParser: true }
+    );
   }
 }
 
