54f8a189c157a3145f53c60f3754a8470df593f4
James Kinyua <jameskinyua590@gmail.com>
2019-08-16 13:28:45 +0300

    refactor: replace the in-memory array database with mongodb

--- order-api/src/controllers/api.ts
+++ order-api/src/controllers/api.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { formatOutput } from '../utility/orderApiUtility';
 
-const APPLICATION_JSON = 'application/json';
-
 export let getApi = (req: Request, res: Response, next: NextFunction) => {
   formatOutput(res, { title: 'Order API' }, 200);
 };
