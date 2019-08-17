28740d275c96c2de45d3aa6880f3ce467b8cbbf0
James Kinyua <jameskinyua590@gmail.com>
2019-08-17 17:39:29 +0300

    chore: configure authentication using jwt and passport

--- order-api/src/controllers/user.ts
+++ order-api/src/controllers/user.ts
@@ -1,7 +1,30 @@
+import * as bcrypt from 'bcrypt';
 import { NextFunction, Request, Response } from 'express';
 import halson = require('halson');
+import * as jwt from 'jsonwebtoken';
 import { UserModel } from '../schemas/User';
 import { formatOutput } from '../utility/orderApiUtility';
+
+export let login = (req: Request, res: Response, next: NextFunction) => {
+  const username = req.query.username;
+  const password = req.query.password;
+
+  UserModel.findOne({ username: username }, (err, user) => {
+    if (!user) {
+      return res.status(404).send();
+    }
+    const validate = bcrypt.compareSync(password, user.password.valueOf());
+    if (validate) {
+      const body = { _id: user._id, email: user.email };
+
+      const token = jwt.sign({ user: body }, 'top_secret');
+
+      return res.json({ token: token });
+    } else {
+      return res.status(401).send();
+    }
+  });
+};
 
 export let getUser = (req: Request, res: Response, next: NextFunction) => {
   const username = req.params.username;
@@ -21,6 +44,8 @@ export let getUser = (req: Request, res: Response, next: NextFunction) => {
 
 export let addUser = (req: Request, res: Response, next: NextFunction) => {
   const newUser = new UserModel(req.body);
+
+  newUser.password = bcrypt.hashSync(newUser.password, 10);
 
   newUser.save((error, user) => {
     user = halson(user.toJSON()).addLink('self', `/users/${user._id}`);
