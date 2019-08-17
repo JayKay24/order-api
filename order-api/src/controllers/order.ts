bedb5b7182954886fe8dcd0c75677dc539140d59
James Kinyua <jameskinyua590@gmail.com>
2019-08-17 23:37:20 +0300

    chore: configure error-handling and logging for the order-api

--- order-api/src/controllers/order.ts
+++ order-api/src/controllers/order.ts
@@ -3,13 +3,18 @@ import halson = require('halson');
 import * as _ from 'lodash';
 import { OrderModel } from '../schemas/Order';
 import { UserModel } from '../schemas/User';
+import { OrderAPILogger } from '../utility/logger';
 import { formatOutput } from '../utility/orderApiUtility';
 
 export let getOrder = (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
+
+  OrderAPILogger.logger.info(`[GET] [/store/orders/] ${id}`);
+
   OrderModel.findById(id, (err, order) => {
     if (!order) {
-      return res.status(404).send();
+      OrderAPILogger.logger.info(`[GET] [/store/orders/:{orderId}] Order ${id} not found.`)
+      return next(new Error(`Order ${id} not found.`));
     }
     order = halson(order.toJSON()).addLink('self', `/store/orders/${order.id}`);
     return formatOutput(res, order, 200, 'order');
@@ -19,6 +24,8 @@ export let getOrder = (req: Request, res: Response, next: NextFunction) => {
 export let getAllOrders = (req: Request, res: Response, next: NextFunction) => {
   const limit = Number(req.query.limit) || 0;
   const offset = Number(req.query.offset) || 0;
+
+  OrderAPILogger.logger.info(`[GET] [/store/orders]`);
 
   OrderModel.find({}, null, { skip: offset, limit: limit }).then(orders => {
     if (orders) {
@@ -37,12 +44,17 @@ export let getAllOrders = (req: Request, res: Response, next: NextFunction) => {
 export let addOrder = (req: Request, res: Response, next: NextFunction) => {
   const userId = req.body.userId;
 
+  OrderAPILogger.logger.info(`[POST] [/store/orders] ${userId}`);
+
   UserModel.findById(userId, (err, user) => {
     if (!user) {
-      return res.status(404).send();
+      OrderAPILogger.logger.info(`[POST] [/store/orders] There is no user with the userId ${userId}`);
+      throw new Error(`There is no user with the userId ${userId}`);
     }
 
     const newOrder = new OrderModel(req.body);
+
+    OrderAPILogger.logger.info(`[POST] [/store/orders] ${newOrder}`);
 
     newOrder.save((error, order) => {
       order = halson(order.toJSON())
@@ -58,8 +70,12 @@ export let addOrder = (req: Request, res: Response, next: NextFunction) => {
 
 export let removeOrder = (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
+
+  OrderAPILogger.logger.warn(`[DELETE] [/store/orders] ${id}`);
+
   OrderModel.findById(id, (err, order) => {
     if (!order) {
+      OrderAPILogger.logger.warn(`[DELETE] [/store/orders:{orderId}] Order id ${id} not found`);
       return res.status(404).send();
     }
     order.remove(error => {
@@ -70,6 +86,9 @@ export let removeOrder = (req: Request, res: Response, next: NextFunction) => {
 
 export let getInventory = (req: Request, res: Response, next: NextFunction) => {
   const status = req.query.status;
+
+  OrderAPILogger.logger.info(`[GET] [/store/inventory] ${status}`);
+
   OrderModel.find({ status: status }, (err, orders) => {
     orders = _.groupBy(orders, 'userId');
     return formatOutput(res, orders, 200, 'inventory');
