2ffaf25998c15d2596050dd1c3715afcbbc33a1e
James Kinyua <jameskinyua590@gmail.com>
2019-08-17 18:49:06 +0300

    refactor: configure email validator for user Schema

--- order-api/test/routes/02_user.spec.ts
+++ order-api/test/routes/02_user.spec.ts
@@ -33,8 +33,6 @@ describe('userRoute', async () => {
       newUser.password = bcrypt.hashSync(newUser.password, 10);
 
       newUser.save(async (error, userCreated) => {
-        // tslint:disable-next-line: no-console
-        console.log('criou');
         user._id = userCreated._id;
         done();
       });
