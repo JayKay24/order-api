0fdeb13ae657b7b805786e442ca377c4a68757b8
James Kinyua <jameskinyua590@gmail.com>
2019-08-17 18:49:06 +0300

    refactor: configure email validator for user Schema

--- order-api/src/schemas/User.ts
+++ order-api/src/schemas/User.ts
@@ -1,16 +1,19 @@
-import { Document, Model, model, Schema } from 'mongoose'
-import { default as User } from '../models/user'
+import { Document, Model, model, Schema } from 'mongoose';
+import uniqueValidator = require('mongoose-unique-validator');
+import { default as User } from '../models/user';
 
 export interface UserModel extends User, Document {}
 
 export const UserSchema: Schema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
+  email: { type: String, unique: true },
   password: String,
   phone: String,
   userStatus: Number,
   username: String,
 })
+
+UserSchema.plugin(uniqueValidator);
 
 export const UserModel: Model<UserModel> = model<UserModel>('User', UserSchema)
