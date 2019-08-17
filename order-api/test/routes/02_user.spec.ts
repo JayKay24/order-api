80433ee6f00a0de953b2d0135ba2b8336825faba
James Kinyua <jameskinyua590@gmail.com>
2019-08-17 17:39:29 +0300

    chore: configure authentication using jwt and passport

--- order-api/test/routes/02_user.spec.ts
+++ order-api/test/routes/02_user.spec.ts
@@ -1,44 +1,72 @@
 'use strict';
 
+import * as bcrypt from 'bcrypt';
 import chai = require('chai');
 import chaiHttp = require('chai-http');
 import 'mocha';
 import app from '../../src/app';
-import User from '../../src/models/User';
 import { UserModel } from '../../src/schemas/User';
 
 chai.use(chaiHttp);
 
 const expect = chai.expect;
 
-const user: User = {
-  username: 'John',
-  firstName: 'John',
-  lastName: 'Doe',
-  email: 'John@myemail.com',
-  password: 'password',
-  phone: '5555555',
-  userStatus: 1,
-};
+describe('userRoute', async () => {
+  const user = {
+    _id: null,
+    username: 'John',
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'John@myemail.com',
+    password: 'password',
+    phone: '5555555',
+    userStatus: 1,
+  };
 
-describe('userRoute', () => {
-  before(async () => {
+  let token;
+
+  before((done) => {
     expect(UserModel.modelName).to.be.equal('User');
-    UserModel.collection.drop();
+
+    UserModel.db.db.dropCollection('users', async (err, result) => {
+      const newUser = new UserModel(user);
+      newUser.password = bcrypt.hashSync(newUser.password, 10);
+
+      newUser.save(async (error, userCreated) => {
+        // tslint:disable-next-line: no-console
+        console.log('criou');
+        user._id = userCreated._id;
+        done();
+      });
+    });    
+  });
+
+  it('should be able to login', () => {
+    return chai
+      .request(app)
+      .get(`/users/login?username=${user.username}&password=${user.password}`)
+      .then(res => {
+        expect(res.status).to.be.equal(200);
+        token = res.body.token;
+      });
   });
 
   it('should respond with HTTP 404 status because there is no user', async () => {
     return chai
       .request(app)
-      .get(`/users/${user.username}`)
+      .get('/users/NO_USER')
+      .set('Authorization', `Bearer ${token}`)
       .then(res => {
         expect(res.status).to.be.equal(404);
       });
   });
   it('should create a new user and retrieve it back', async () => {
+    user.email = 'unique_email@example.com';
+
     return chai
       .request(app)
-      .post('/users/')
+      .post('/users')
+      .set('Authorization', `Bearer ${token}`)
       .send(user)
       .then(res => {
         expect(res.status).to.be.equal(201);
@@ -49,6 +77,7 @@ describe('userRoute', () => {
     return chai
       .request(app)
       .get(`/users/${user.username}`)
+      .set('Authorization', `Bearer ${token}`)
       .then(res => {
         expect(res.status).to.be.equal(200);
         expect(res.body.username).to.be.equal(user.username);
@@ -66,6 +95,7 @@ describe('userRoute', () => {
     return chai
       .request(app)
       .patch(`/users/John`)
+      .set('Authorization', `Bearer ${token}`)
       .send(user)
       .then(res => {
         expect(res.status).to.be.equal(204);
@@ -75,6 +105,7 @@ describe('userRoute', () => {
     return chai
       .request(app)
       .get(`/users/${user.username}`)
+      .set('Authorization', `Bearer ${token}`)
       .then(res => {
         expect(res.status).to.be.equal(200);
         expect(res.body.username).to.be.equal(user.username);
@@ -92,6 +123,7 @@ describe('userRoute', () => {
     return chai
       .request(app)
       .patch(`/users/Mary`)
+      .set('Authorization', `Bearer ${token}`)
       .send(user)
       .then(res => {
         expect(res.status).to.be.equal(404);
@@ -101,14 +133,16 @@ describe('userRoute', () => {
     return chai
       .request(app)
       .del(`/users/${user.username}`)
+      .set('Authorization', `Bearer ${token}`)
       .then(res => {
         expect(res.status).to.be.equal(204);
       });
   });
-  it('should return 404 when it is trying to remove an user because the user does not exist', async () => {
+  it('should return 404 when it is trying to remove a user because the user does not exist', async () => {
     return chai
       .request(app)
       .del(`/users/Mary`)
+      .set('Authorization', `Bearer ${token}`)
       .then(res => {
         expect(res.status).to.be.equal(404);
       });
